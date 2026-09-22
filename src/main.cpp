e95e42cde8cbc9d55653a0be1ccac04b9dec6850
relyks7 <skylerhuahuayeye@gmail.com>
2026-09-22 16:27:28 -0400

    -Added basic drivetrain (uncalibrated)

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "lemlib/api.hpp"
 using namespace pros;
 using namespace lemlib;
+
+/// unset params
 //motor ports, +/- for direction
 int p_l1=0;
 int p_l2=0;
@@ -9,16 +11,98 @@ int p_r1=0;
 int p_r2=0;
 //imu port
 int p_imu=0;
+//tracking wheel encoders, horizontal and vertical, - if reversed
+int p_h_sens=0;
+int p_v_sens=0;
+//tracking wheels - offsets
+double oset_h=0;
+double oset_v=0;
+///
+
+/// set params
+//track width (in)
+int w_track=13;
+//drivetrain wheel
+float wheel_drive=Omniwheel::NEW_325;
+//drivetrain rpm
+double rpm_drive=600;
+//drivetrain horizontal drift
+double h_drift_drive=2;
+///
+
+/// drivetrain
 //left and right motorgroups
 MotorGroup left({p_l1, p_l2}, v5::MotorGears::blue);
 MotorGroup right({p_r1, p_l2}, v5::MotorGears::blue);
-Imu imu(p_imu);
-Drivetrain drivetrain(
+Drivetrain drive(
 	&left,
 	&right,
-	13,
-	Omniwheel::NEW_325,
-	600,
-	2
+	w_track,
+	wheel_drive,
+	rpm_drive,
+	h_drift_drive
 );
-Imu imu(p_imu);
+Imu imu(p_imu); //intertial measurement unit;
+//tracking wheels + sensors + odom
+Rotation h_sens(p_h_sens);
+Rotation v_sens(p_v_sens);
+TrackingWheel h_track_wheel(&h_sens, Omniwheel::NEW_2, oset_h);
+TrackingWheel v_track_wheel(&v_sens, Omniwheel::NEW_2, oset_v);
+OdomSensors odom_sens(
+	&v_track_wheel,
+	nullptr,
+	&h_track_wheel,
+	nullptr,
+	&imu
+);
+///
+
+/// pid
+//lateral controller
+ControllerSettings control_lat(
+	10, //k_p
+	0, //k_i
+	3, //k_d
+	3, //anti windup
+	1, //small error range, dist
+	100, //small error range, time
+	3, //large error range, dist 
+	500, //large error range, time
+	20 //max acceleration
+);
+//angular controller
+ControllerSettings control_ang(
+	2, //k_p
+	0, //k_i
+	10, //k_d
+	3, //anti windup
+	1, //small error range, dist
+	100, //small error range, time
+	3, //large error range, dist 
+	500, //large error range, time
+	0 //max acceleration
+);
+///
+
+/// chassis
+Chassis chassis(drive, control_lat, control_ang, odom_sens);
+///
+
+/// init
+void initialize(){
+	lcd::initialize;
+	chassis.calibrate();
+	Task screen_task(
+		[&](){
+			while (true){
+				lcd::print(0, "x: %f", chassis.getPose().x);
+				lcd::print(1, "y: %f", chassis.getPose().y);
+				lcd::print(2, "yheta: %f", chassis.getPose().theta);
+				delay(20);
+			}
+		}
+	);
+}
+///
+
+//~relyks
